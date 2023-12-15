8b468b0518e80edd1e4d0ee321fbb9d4d5752eb3
vlnk <vlnk@protonmail.com>
2023-12-15 12:32:30 -0500

    🚨 lint all the components

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ function SunnyButton () {
   const { darkTheme, toggleTheme } = useContext(DarkThemeContext)
 
   return (
-    <MdSunny onClick={toggleTheme}
-             className={`bg-gray-300 cursor-pointer dark:bg-gray-700 p-2 rounded-lg  bottom-5 right-5 ${
+    <MdSunny
+      onClick={toggleTheme}
+      className={`bg-gray-300 cursor-pointer dark:bg-gray-700 p-2 rounded-lg  bottom-5 right-5 ${
                 darkTheme ? 'text-white' : 'text-black'
               }`}
-             size={32}
+      size={32}
     />
   )
 }
@@ -35,10 +36,9 @@ function App () {
   const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem('tasks')) || [])
   const { darkTheme, toggleTheme } = useContext(DarkThemeContext)
 
-
   useEffect(() => {
     localStorage.setItem('tasks', JSON.stringify(tasks))
-  }, [tasks]);
+  }, [tasks])
 
   const addTask = (title) => {
     const newTask = { id: Date.now(), title, completed: false }
@@ -73,21 +73,22 @@ function App () {
   const getCompletedTasks = () => tasks.filter((task) => task.completed)
   const getRemainingTasks = () => tasks.filter((task) => !task.completed)
 
-
   return (
-    <div id="App"
-         className={`hero
+    <div
+      id='App'
+      className={`hero
                      ${darkTheme ? 'bg-gray-900' : 'bg-gray-100'}
                      h-screen md:min-h-[700px]
                      w-screen m-auto
                      flex flex-col items-center
-                     transition-all duration-500`}>
+                     transition-all duration-500`}
+    >
       <div className={`flex flex-col space-y-6 w-[600px] md:w-[100%] z-10 p-4 ${
           darkTheme ? 'text-gray-100' : 'text-slate-700'
         }`}
       >
         <header className='w-full flex items-center justify-between'>
-          <h1 className={`uppercase text-4xl font-bold tracking-widest mb-4 md:text-3xl`}>
+          <h1 className='uppercase text-4xl font-bold tracking-widest mb-4 md:text-3xl'>
             Tasks
           </h1>
 
@@ -113,19 +114,19 @@ function App () {
           </div>
 
           {tasks.length
-           ? (
-             <TaskList
-               tasks={tasks}
-               onEditTask={editTask}
-               onDeleteTask={deleteTask}
-               onToggleCompleted={toggleCompleted}
-             />
-           )
-           : (
-             <div className=' w-full h-[80%] flex items-center justify-center overflow-hidden'>
-               <p className=' text-gray-500 text-center z-10'>Empty task</p>
-             </div>
-           )}
+            ? (
+              <TaskList
+                tasks={tasks}
+                onEditTask={editTask}
+                onDeleteTask={deleteTask}
+                onToggleCompleted={toggleCompleted}
+              />
+              )
+            : (
+              <div className=' w-full h-[80%] flex items-center justify-center overflow-hidden'>
+                <p className=' text-gray-500 text-center z-10'>Empty task</p>
+              </div>
+              )}
         </div>
       </div>
     </div>
